16e0927ec35a41e65ce80723aa5ec7330d9dd3e2
C201127_Md. Sayem Uddin <90597295+SayemUddin@users.noreply.github.com>
2022-11-19 22:07:05 +0600

    done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+/*(function ($) {
+    "use strict";
+
+//Navbar on scrolling
+$(window).scroll(function(){
+    if($(this).scrollTop()>1){
+        $('.navbar').fadeIn('slow').css('display','flex');
+    }else{
+        $('.navbar').fadeOut('slow').css('display','none');
+    }
+});
+})(jQuery);*/
